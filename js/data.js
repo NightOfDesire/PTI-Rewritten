4e21c23fa81a798857b5c02f3079a151d6dd7957
WorldsWorstDeveloper <162475825+WorldsWorstDeveloper@users.noreply.github.com>
2024-03-08 21:18:25 +0000

    t

--- js/data.js
+++ js/data.js
@@ -127,13 +127,13 @@ function loadGame(start=true, gotNaN=false) {
     
     if (start) {
         setupHTML()
-        setupTooltips()
+        //setupTooltips()
 
         setInterval(save,15000)
 
 
         updateHTML()
-        updateTooltipResHTML(true)
+        //updateTooltipResHTML(true)
 
        
 
