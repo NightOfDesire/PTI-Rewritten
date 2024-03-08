3fcf908244c24e8119bdf9aba674c3a74e40577e
WorldsWorstDeveloper <162475825+WorldsWorstDeveloper@users.noreply.github.com>
2024-03-08 22:08:51 +0000

    t

--- js/elements.js
+++ js/elements.js
@@ -9,12 +9,12 @@ function setupHTML() {
 
 
 
-	tmp.el = {}
+	/*tmp.el = {}
 	let all = document.getElementsByTagName("*")
 	for (let i=0;i<all.length;i++) {
 		let x = all[i]
 		tmp.el[x.id] = new Element(x)
-	}
+	}*/
 }
     
 
