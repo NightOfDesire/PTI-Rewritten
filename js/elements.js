bdf731378b5a4a19ee2a5b506d66762e0869653d
WorldsWorstDeveloper <162475825+WorldsWorstDeveloper@users.noreply.github.com>
2024-03-09 20:08:46 +0000

    txt

--- js/elements.js
+++ js/elements.js
@@ -37,8 +37,8 @@ function updateHTML() {
 	
 
 	updateResourcesHTML()
-	tmp.el.loading.setDisplay(!tmp.start)
-	tmp.el.app.setDisplay(tmp.start)
+	//tmp.el.loading.setDisplay(!tmp.start)
+	//tmp.el.app.setDisplay(tmp.start)
 	if (hover_tooltip) updateTooltipResHTML()
 
 	
