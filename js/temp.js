4baead73150db70c8e5d1ad1c5da1b7aa9712121
WorldsWorstDeveloper <162475825+WorldsWorstDeveloper@users.noreply.github.com>
2024-03-11 18:12:39 +0000

    t

--- js/temp.js
+++ js/temp.js
@@ -58,6 +58,7 @@ function updateNumTemp() {
     tmp.passiveNumberGain = player.number.gte("1e11750")
 }
 function updateTemp() {
+    if (!tmp.stab) tmp.stab = []
     tmp.offlineActive = player.offline.time > 1
     tmp.offlineMult = tmp.offlineActive?player.offline.time+1:1
     tmp.gs = FORMS.gamespeed()
