55fac3c8272da7ac3ccf16646bdd672d65071b2f
WorldsWorstDeveloper <162475825+WorldsWorstDeveloper@users.noreply.github.com>
2024-03-11 18:10:05 +0000

    t

--- js/elements.js
+++ js/elements.js
@@ -53,8 +53,8 @@ function updateTabsHTML() {
 			tmp.el["stabs"+x].setDisplay(x == tmp.tab)
 			if (x == tmp.tab) for (let y = 0; y < TABS[2][x].length; y++)  {
 				let stab = TABS[2][x][y]
-				tmp.el["stab"+x+"_"+y].setDisplay(stab.unl ? stab.unl() : true)
-				tmp.el["stab"+x+"_"+y].setClasses({btn_tab: true, [stab.style ? stab.style : "normal"]: true, choosed: y == tmp.stab[x]})
+				//tmp.el["stab"+x+"_"+y].setDisplay(stab.unl ? stab.unl() : true)
+				//tmp.el["stab"+x+"_"+y].setClasses({btn_tab: true, [stab.style ? stab.style : "normal"]: true, choosed: y == tmp.stab[x]})
 				if (tmp.el["stab_frame"+x+"_"+y]) tmp.el["stab_frame"+x+"_"+y].setDisplay(y == tmp.stab[x])
 			}
 		}
@@ -63,9 +63,7 @@ function updateTabsHTML() {
 
 
 
-/*function navhide(opt) {
-	player.options.hidenav[opt] = !player.options.hidenav[opt]
-}*/
+
 function hideNavigation(i) { player.options.nav_hide[i] = !player.options.nav_hide[i]; updateNavigation() }
 
 function updateNavigation() {
