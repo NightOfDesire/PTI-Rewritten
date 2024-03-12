eb51f2dc133ac05c2a32aa68b65a089310a9f0b8
WorldsWorstDeveloper <162475825+WorldsWorstDeveloper@users.noreply.github.com>
2024-03-12 01:13:17 +0000

    xt

--- js/elements.js
+++ js/elements.js
@@ -54,7 +54,7 @@ function updateTabsHTML() {
 		if (TABS[2][x]) {
 			tmp.el["stabs"+x].setDisplay(x == tmp.tab)
 			if (x == tmp.tab) for (let y = 0; y < TABS[2][x].length; y++)  {
-				let stab = TABS[2][x][y]
+				//let stab = TABS[2][x][y]
 				//tmp.el["stab"+x+"_"+y].setDisplay(stab.unl ? stab.unl() : true)
 				//tmp.el["stab"+x+"_"+y].setClasses({btn_tab: true, [stab.style ? stab.style : "normal"]: true, choosed: y == tmp.stab[x]})
 				// if (tmp.el["stab_frame"+x+"_"+y]) tmp.el["stab_frame"+x+"_"+y].setDisplay(y == tmp.stab[x])
