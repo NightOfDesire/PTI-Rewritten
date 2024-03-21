acaa7aa9c95a3b42a5653839fbaa1553733c3f8c
WorldsWorstDeveloper <162475825+WorldsWorstDeveloper@users.noreply.github.com>
2024-03-21 20:51:20 +0000

    t

--- js/data.js
+++ js/data.js
@@ -203,7 +203,7 @@ function importy() {
     createPrompt("Paste in your save WARNING: WILL OVERWRITE YOUR CURRENT SAVE",'import',loadgame=>{
         let st = ""
         if (loadgame.length <= 100) st = convertStringIntoAGY(loadgame)
-        if (ssf[2](loadgame)) return
+        //if (ssf[2](loadgame)) return
         if (st == 'OJY$VFe*b') {
             addNotify('monke<br><img style="width: 100%; height: 100%" src="https://i.kym-cdn.com/photos/images/original/001/132/314/cbc.jpg">')
             return
