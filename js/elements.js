6af4e32a3cb618b70f9cf388ccebf2598ee15e8e
WorldsWorstDeveloper <162475825+WorldsWorstDeveloper@users.noreply.github.com>
2024-03-21 05:25:10 +0000

    r

--- js/elements.js
+++ js/elements.js
@@ -26,11 +26,14 @@ function updateHTML() {
 	tmp.el.loading.setDisplay(!tmp.start)
 	tmp.el.app.setDisplay(tmp.start)
     tmp.el.Essence.setHTML(`
-    <img src="images/essence.png"><br>Essence: ${format(player.essence)} ${formatGain(player.essence, tmp.essenceGain.mul(tmp.gs))}   <br> <br> <br>
+    <br>Essence: ${format(player.essence)} ${formatGain(player.essence, tmp.essenceGain.mul(tmp.gs))}   <br> <br> <br>
     `)
 
 	tmp.el.Prestige.setHTML(tmp.pres.can?`<i><b>Prestige</b> and reset your essence for +<b>${format(tmp.pres.gain)} Prestige Shards</i>`:`<i>Locked.</i>`)
 	tmp.el.prespts.setHTML(`
 	<br>Prestige Shards: ${format(player.pres.pts)} (${formatGain(tmp.pres.gain)})
 	`)
+
+
+	updateRanksHTML()
 }
---
+++ js/ranks.js
@@ -0,0 +1,27 @@
+const RANKS = {
+    reset(type) {
+        switch(type) {
+            case "rank": 
+
+            player.essence = E(0)
+            player.pres.pts = E(0)
+            player.ranks.rank = player.ranks.rank.add(1)
+            addNotify(`Ranked up to ${format(player.ranks.rank.add(1), 0)}.`, 1)
+            default: ''
+        }
+    },
+    text: {
+        "1": `Prestige Shard gain is raised by 1.15, ${formatMult(2)} Essence`
+    },
+    effects: {
+        
+    }
+}
+
+function updateRanksHTML() {
+
+    tmp.el.rankup.setHTML(`
+    Reset your progress but rank up. ${RANKS.text[player.ranks.rank.add(1)] ? `At rank ${format(player.ranks.rank.add(1), 0)} - ${RANKS.text[player.ranks.rank.add(1)]}` : ""}
+    `)
+
+}
