2acd6f6724f1556f306e06115fade0b0f9968a36
WorldsWorstDeveloper <162475825+WorldsWorstDeveloper@users.noreply.github.com>
2024-03-21 23:52:29 +0000

    t

--- js/eclipse.js
+++ js/eclipse.js
@@ -1,23 +1,24 @@
 const ECLIPSE = {
     activate() {
-        player.eclipse.active ? this.DEACTIVATE() : this.ACTIVATE
+        player.eclipse.active ? this.DEACTIVATE() : this.ACTIVATE()
     },
     ACTIVATE() {
-        player.eclipse.active = true
         player.essence = E(0)
         player.pres.pts = E(0)
         player.ranks.rank = E(0)
+        player.eclipse.active = true
         document.body.style.backgroundColor = "orange"
         addNotify(`You feel strange.. and it seems your resources are much harder to acquire.`,10)
     },
     DEACTIVATE() {
-        setTimeout(1250)
+        if (player.eclipse.active) {
         if (player.essence.gte('1e20')) {
             player.eclipse.shards = player.eclipse.shards.add(this.shardGain())
         }
         player.eclipse.active = false
         document.body.style.backgroundColor = "hsl(0, 0%, 7%)"
         addNotify(`It feels much colder now.. and y(our resources seem to be back to normal.`)
+    }
     },
     ACTIVE() {
         return player.eclipse.active
