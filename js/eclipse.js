70157f3c15cb0e7b61bc301e5804734280086edb
WorldsWorstDeveloper <162475825+WorldsWorstDeveloper@users.noreply.github.com>
2024-03-22 00:03:47 +0000

    t

--- js/eclipse.js
+++ js/eclipse.js
@@ -6,28 +6,27 @@ const ECLIPSE = {
         ECLIPSE.ACTIVATE()
     },
     ACTIVATE() {
+        if (!player.eclipse.active) {
         player.essence = E(0)
         player.pres.pts = E(0)
         player.ranks.rank = E(0)
         player.eclipse.active = true
         document.body.style.backgroundColor = "orange"
         addNotify(`You feel strange.. and it seems your resources are much harder to acquire.`,10)
-    },
-    DEACTIVATE() {
-        if (player.eclipse.active) {
-        if (player.essence.gte('1e20')) {
-            player.eclipse.shards = player.eclipse.shards.add(this.shardGain())
+        } else {
+            if (player.essence.gte('1e20')) {
+                player.eclipse.shards = player.eclipse.shards.add(ECLIPSE.shardGain())
+            }
+            player.eclipse.active = false
+            document.body.style.backgroundColor = "hsl(0, 0%, 7%);"
+            addNotify(`It feels much colder now.. and your resources appear to be back to normal.`,10)
         }
-        player.eclipse.active = false
-        document.body.style.backgroundColor = "hsl(0, 0%, 7%)"
-        addNotify(`It feels much colder now.. and y(our resources seem to be back to normal.`)
-    }
     },
     ACTIVE() {
         return player.eclipse.active
     },
     shardGain() {
-        let sg = player.essence.pow(2).root(666)
+        let sg = player.essence.pow(0.0736).div(879.022517)
 
         return sg
     }
