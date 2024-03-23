70b8a45ec9084ee8344e1bde2a997e02e310dbb3
Night <164546511+NightOfDesire@users.noreply.github.com>
2024-03-23 16:08:01 +0000

    t

--- js/elements.js
+++ js/elements.js
@@ -10,11 +10,13 @@ function setupHTML() {
 	for (let i = 0; i < RANKS.names.length; i++) {
 		let type = RANKS.names[i]
 		table += `
-		<div id="${type}" class="charged_text"></div>
+		<div id="${type_div}">
+		<span id="${type}" class="charged_text"></div>
 		<button id="${type}_btn" onclick="RANKS.reset('${type}')" class="btn" style="min-width: 250px; min-height: 130px;">
-		<span id="${type_base}">Reset your progress, but ${type} up for a powerful boost.</span>
+		<span id="${type}_msg">Reset your progress, but ${type} up for a powerful boost.</span>
 		<span id="${type}_desc"></span>
 		</button>
+		</div>
 		`
 	}
 	/*for (let x = 0; x < RANKS.names.length; x++) {
