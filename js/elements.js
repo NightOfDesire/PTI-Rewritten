b9fddd7af8422d4fddcbfc48eff19423147855a4
Night <164546511+NightOfDesire@users.noreply.github.com>
2024-03-24 19:08:00 +0000

    t

--- js/elements.js
+++ js/elements.js
@@ -5,6 +5,7 @@ function setupHTML() {
 	//setupModsHTML()
 
 	TAB_TERMINAL.setup()
+	//there is NO buyables line here!
 	let rank_table = new Element('rank_table')
 	for (let i = 0; i < RANKS.names.length; i++) {
 		let rn = RANKS.names[i]
