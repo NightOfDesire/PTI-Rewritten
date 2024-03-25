d31ca1eade7349e02f4186bd72a6eb80dd441356
Night <164546511+NightOfDesire@users.noreply.github.com>
2024-03-25 03:59:17 +0000

    t

--- js/supernova/upgrades.js
+++ js/supernova/upgrades.js
@@ -127,5 +127,8 @@ function resetMainUpgs(id,keep=[]) {
     player.mainUpg[id2] = k
 }
 function upgEffect(id,x) {
-    return UPGRADES[UPGRADES.ids.indexOf(id)][x].effect()
+    const idlist = {
+        stars: '1'
+    }
+    return UPGRADES[idlist[id]][x].effect()
 }
