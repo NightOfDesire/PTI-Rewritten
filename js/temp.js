fc89a15c8db4a05ca7ca3604b2b2e87379a37a4e
Night <164546511+NightOfDesire@users.noreply.github.com>
2024-03-26 01:31:34 +0000

    t

--- js/temp.js
+++ js/temp.js
@@ -44,8 +44,6 @@ function updateTemp() {
     if (!tmp.stab) tmp.stab = []
     if (!tmp.notify) tmp.notify = []
     if (!tmp.popup) tmp.popup = []
-    if (player.essence.gte(player.misc.hEss)) player.misc.hEss = player.essence
-    if (player.pres.pts.gte(player.misc.hEss)) player.misc.hPres = player.pres.pts
     tmp.offlineActive = player.offline.time > 1
     tmp.offlineMult = tmp.offlineActive?player.offline.time+1:1
     tmp.gs = E(1).mul(player.devoptions.speed)
