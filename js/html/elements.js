cb2ceaf25136ba18301c57f0f7d505ea48e4f90d
Night <164546511+NightOfDesire@users.noreply.github.com>
2024-03-30 14:14:46 +0000

    t

--- js/html/elements.js
+++ js/html/elements.js
@@ -6,7 +6,7 @@ function setupHTML() {
 	let table2 = ""
 	for (let x = 0; x < TABS[1].length; x++) {
 		table += `<div>
-			<button onclick="TABS.choose(${x});player.stab=0;" class="btn_tab" id="tab${x}"><div>${TABS[1][x].id}</div></button>
+			<button onclick="TABS.choose(${x})" class="btn_tab" id="tab${x}"><div>${TABS[1][x].id}</div></button>
 		</div>`
 		if (TABS[2][x]) {
 			let a = `<div id="stabs${x}" class="table_center stab_btn">`
@@ -31,43 +31,31 @@ function setupHTML() {
 
 	
 }
-
-
-
 function updateTabsHTML() {
-	tmp.el.stabs_div.setDisplay(TABS[2][player.tab])
-	for (let x=0;x<TABS[1].length;x++) {
-		let t = TABS[1][x]
-		if (tmp.el["tab_"+x]) tmp.el["tab_"+x].setDisplay(player.tab == x)
-		if (TABS[2][x]) {
-			tmp.el["stabs"+x].setDisplay(player.tab == x)
-			if (player.tab == x) for (let y=0;x<TABS[2][x].length;y++) {
-				let s = TABS[2][x][y]
-				/**@param hello */
-				tmp.el["stab"+x+"_"+y].setDisplay(s.unl ? s.unl() : true)
-				/**@param error */
-				tmp.el["stab"+x+"_"+y].setClasses({btn_tab: true, [s.style ? s.style : "normal"]: true, choosed: player.stab == y})
-				if (tmp.el["stab_frame"+x+"_"+y]) tmp.el["stab_frame"+x+"_"+y].setDisplay(player.stab == y)
-			}
-		}
-	}
-	/*tmp.el.stabs_div.setDisplay(TABS[2][player.tab])
+	let s = true
+	tmp.el.stabs_div.setDisplay(TABS[2][tmp.tab])
 	
 	for (let x = 0; x < TABS[1].length; x++) {
 		let tab = TABS[1][x]
+		if (s) {
+			tmp.el["tab"+x].setDisplay(tab.unl ? tab.unl() : true)
+			tmp.el["tab"+x].setClasses({btn_tab: true, [tab.style ? tab.style : "normal"]: true, choosed: x == tmp.tab})
+		}
 
-		if (tmp.el["tab_frame"+x]) tmp.el["tab_frame"+x].setDisplay(x == player.tab)
+		if (tmp.el["tab_"+x]) tmp.el["tab_"+x].setDisplay(x == tmp.tab)
 		if (TABS[2][x]) {
-			tmp.el["stabs"+x].setDisplay(x == player.tab)
-			if (x == player.tab) for (let y = 0; y < TABS[2][x].length; y++)  {
+			tmp.el["stabs"+x].setDisplay(x == tmp.tab)
+			if (x == tmp.tab) for (let y = 0; y < TABS[2][x].length; y++)  {
 				let stab = TABS[2][x][y]
 				tmp.el["stab"+x+"_"+y].setDisplay(stab.unl ? stab.unl() : true)
-				tmp.el["stab"+x+"_"+y].setClasses({btn_tab: true, [stab.style ? stab.style : "normal"]: true, choosed: y == player.stab[x]})
-				if (tmp.el["stab_frame"+x+"_"+y]) tmp.el["stab_frame"+x+"_"+y].setDisplay(y == player.stab[x])
+				tmp.el["stab"+x+"_"+y].setClasses({btn_tab: true, [stab.style ? stab.style : "normal"]: true, choosed: y == tmp.stab[x]})
+				if (tmp.el["stab_frame"+x+"_"+y]) tmp.el["stab_frame"+x+"_"+y].setDisplay(y == tmp.stab[x])
 			}
 		}
-	}*/
+	}
 }
+
+
 
 function updateHTML() {
 
--- js/html/tabs.js
+++ js/html/tabs.js
@@ -1,16 +1,23 @@
 const TABS = {
-    choose(x, stab=false) {if (!stab) {player.tab = x} else {player.stab = x}},
-    1: [
-        {id: "Main"},
-        {id: "Settings"}
+    choose(x, stab=false) {
+        if (!stab) {
+            tmp.tab = x
+        } else {
+            tmp.stab[tmp.tab] = x
+        }
+    },
+    1: [ 
+        {id:"Main"}, 
+        {id:"Settings"}
     ],
     2: {
         0: [
-            {id: "Main"},
-            {id: "Prestige", unl() { return player.pts.gte(500) || player.prestige.unl}}
+            {id:"Main"},
+            {id:"Prestige", icon: "material-symbols:star", color: "cyan", unl() {return true}}
         ],
         1: [
-            {id: "Settings"}
+            {id:"Options"}
         ]
     }
+    
 }
