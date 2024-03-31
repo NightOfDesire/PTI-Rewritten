a4e78141b66a0a4e94e53286fef16d2d83c899e6
Night <164546511+NightOfDesire@users.noreply.github.com>
2024-03-31 00:10:36 +0000

    t

--- js/html/elements.js
+++ js/html/elements.js
@@ -12,7 +12,6 @@ function setupHTML() {
 			for (let y = 0; y < TABS[2][x].length; y++) {
 				a += `<div style="width: 120px">
 					<button style="width: 120px" onclick="TABS.choose(${y}, true)" class="btn_tab" id="stab${x}_${y}">${TABS[2][x][y].id}</button>
-
 				</div>`
 			}
 			a += `</div>`
