d994d26d64a2d4c970b9dcb09f431e097abf9e00
Night <164546511+NightOfDesire@users.noreply.github.com>
2024-04-01 01:42:31 +0000

    t

--- js/html/popups.js
+++ js/html/popups.js
@@ -12,6 +12,23 @@ function updatePopupIndex() {
     popupIndex = i
 }
 
+const POPUP_GROUPS = {
+    help_time: {
+        html: `
+        1 minute = 60 seconds<br>
+        1 hour = 60 minutes<br>
+        1 day = 24hrs<br>
+        1 yr = 365d<br>
+        1 Stellar Year = ${format(1e6)} yr<br>
+        1 Eon = ${format(1e9)} yr<br>
+        1 Eclipsal = ${format("ee3")} yr (logarithmic)<br>
+        1 Gravitational Wavespan = ${format("e30")} Eclipsals (logarithmic)<br>
+        1 Chrono-Resonance Interval = ${format("e100")} Gravitational Wavespans (logarithmic)<br>
+        1 Time-Dilation Drift = ${format("ee6")} Chrono-Resonance Intervals (logarithmic)
+        `
+    }
+}
+
 function addNotify(text, duration=3) {
     tmp.notify.push({text: text, duration: duration});
     if (tmp.notify.length == 1) updateNotify()
@@ -162,26 +179,10 @@ function convertStringIntoAGY(s) {
     }
 
     while (r > 0n) {
-        result += ca[r % cl]
+result += ca[r % cl]
 
         r /= cl
     }
 
     return result
-}
-
-const HELP_HTML = {
-    time: 
-    `
-    1 minute = 60 seconds<br>
-    1 hour = 60 minutes<br>
-    1 day = 24hrs<br>
-    1 yr = 365d<br>
-    1 Stellar Year = ${format(1e6)} yr<br>
-    1 Eon = ${format(1e9)} yr<br>
-    1 Eclipsal = ${format("ee3")} yr (logarithmic)<br>
-    1 Gravitational Wavespan = ${format("e30")} Eclipsals (logarithmic)<br>
-    1 Chrono-Resonance Interval = ${format("e100")} Gravitational Wavespans (logarithmic)<br>
-    1 Time-Dilation Drift = ${format("ee6")} Chrono-Resonance Intervals (logarithmic)
-    `
 }
