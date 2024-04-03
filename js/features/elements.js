57234e6ba77c1316936d8b92cb3169370e2a9c50
Night <164546511+NightOfDesire@users.noreply.github.com>
2024-04-03 19:22:09 +0000

    t

--- js/features/elements.js
+++ js/features/elements.js
@@ -137,7 +137,7 @@ function updateElementsTemp() {
     if (!tmp.elem) tmp.elem = {}
     let tElem = tmp.elem
     tmp.elem.choseElem = player.chosenElem > 0
-    for (let x = tElem.upg_length; x >= 1; x--) if (ELEMENTS.upgs[x].effect) {
+    for (let x = tElem.unl_length; x >= 1; x--) if (ELEMENTS.upgs[x].effect) {
         tElem.effect[x] = ELEMENTS.upgs[x].effect()
     }
 
