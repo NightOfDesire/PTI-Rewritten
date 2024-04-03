93ecd0157a2d56c633a762a3710e361efe5efda6
Night <164546511+NightOfDesire@users.noreply.github.com>
2024-04-03 01:59:23 +0000

    t

--- js/features/time.js
+++ js/features/time.js
@@ -2,6 +2,7 @@ const TIME = {
     updateHTML() {
         tmp.el.TheTime.setHTML(`Universe Age: ${formatTime(player.time.amt)}`)
 
-        if (player.tier.gte(1)) player.time.unl = true
+        /**@param whoops. */
+        if (player.ranks.tier.gte(1)) player.time.unl = true
     }
 }
