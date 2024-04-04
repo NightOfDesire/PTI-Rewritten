59fb572a27d330f6f8fdfb05e717539b90d38cc2
Night <164546511+NightOfDesire@users.noreply.github.com>
2024-04-04 18:34:46 +0000

    t

--- js/html/elements.js
+++ js/html/elements.js
@@ -148,7 +148,7 @@ function getScalingName(name, x=0, y=0) {
 			return capitalFirst(Object.keys(SCALE_START)[n]) + (n%4==3?"-":" ");
 	}
 	return current;
-	
+
 }
 
 function getScalingStart(type, name) {
@@ -338,7 +338,7 @@ function setupHTML() {
 		for (let y = 1; y <= UPGS.main[x].lens; y++) {
 			let key = UPGS.main[x][y]
 			table += `<img onclick="UPGS.main[${x}].buy(${y})" onmouseover="UPGS.main.over(${x},${y})" onmouseleave="UPGS.main.reset()"
-			 style="margin: 3px;" class="img_btn" id="main_upg_${x}_${y}" src="images/${key.img||mark}.png">`
+			 style="margin: 3px;" class="img_btn" id="main_upg_${x}_${y}" src="images/${key.icon||"mark"}.png">`
 		}
 		table += `</div><br><button id="main_upg_${x}_auto" class="btn" style="width: 80px;" onclick="player.auto_mainUpg.${id} = !player.auto_mainUpg.${id}">OFF</button></div>`
 	}
@@ -441,7 +441,7 @@ function updateHTML() {
 	updateRanksHTML()
 	updateStatsHTML()
 	PRESTIGE.updateHTML()
-	if (player.tab == 0) updateMainUpgradesHTML()
+	if (player.stab[2] == 1) updateMainUpgradesHTML()
 	if (player.stab[4] == 0) updateRanksRewardHTML()
 	if (player.stab[4] == 1) updateScalingHTML()
 	if (player.stab[3] == 1) updateElementsHTML()
